cbf02ee3f3640e720866ac2c05fa09ffad409ba0
Wiktor <victortkd88@gmail.com>
2022-08-17 16:01:08 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const botone = document.getElementById('button')
+const valore = document.getElementById('body')
+
+botone.addEventListener('click', (e) => {
+  e.preventDefault()
+  valore.innerHTML = `<h1>GAY POR SAPO</h1>`
+})
+
+
+
